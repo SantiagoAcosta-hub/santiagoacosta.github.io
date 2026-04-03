ecc256718df7353e449e9efa8b4305c0a113da7e
SantiagoAcosta-hub <a.antibohorquez@gmail.com>
2026-04-03 14:53:24 -0300

    Combined tables for analysis covid.sql

--- Combined tables covid.sql
+++ Combined tables covid.sql
@@ -1,16 +1,22 @@
 CREATE OR REPLACE VIEW coviddeaths1.covid.deaths_vaccines AS(
-  select d.continent,
+  SELECT d.continent,
 d.country,
 d.total_cases,
 d.total_deaths,
 v.people_vaccinated,
-v.people_fully_vaccinated,
+v.people_fully_vaccinated AS fully_vaccinated,
 d.fatality_rate,
 d.death_percent_pop,
 d.percentage_infected,
 d.population,
 d.gdp_per_capita
-from coviddeaths1.covid.analysis_data as d
-left join coviddeaths1.covid.analysis_vac as v
-on d.country = v.country
-order by total_deaths DESC)
+FROM coviddeaths1.covid.deaths_analysis as d
+LEFT JOIN coviddeaths1.covid.vaccines_analysis as v
+ON d.country = v.country
+WHERE v.people_vaccinated IS NOT NULL
+AND d.gdp_per_capita IS NOT NULL
+ORDER BY total_deaths DESC
+)
+--------------------------------------------
+SELECT *
+from coviddeaths1.covid.deaths_vaccines
